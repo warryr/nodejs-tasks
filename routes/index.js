5ab040b6609f56d97ed39bdcb3ba3663387a937e
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-06-12 11:35:31 +0300

    feat: add hello-world task

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  if (req.query.name) {
+    res.render('index', {title: 'Express', name: req.query.name});
+  } else {
+    res.status(400);
+    res.json({ message: 'Missing name parameter' });
+  }
 });
 
 module.exports = router;
