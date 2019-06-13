83d35496b6a37c0743026701632839ce4460d505
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-06-13 11:34:45 +0300

    refactor: refactor app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var createError = require('http-errors');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -9,31 +8,25 @@ var filmsRouter = require('./routes/films');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/hello-world', indexRouter);
 app.use('/api/films', filmsRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
