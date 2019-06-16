94b9c882ecea72fa2b02de401a9febd8c78539cf
warryr <heartsyrop@gmail.com>
2019-06-16 22:34:17 +0300

    feat: add connection to mongoDB

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
 var express = require('express');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
 var createError = require('http-errors');
 var logger = require('morgan');
+
+var CONNECTION_URL = 'mongodb+srv://admin:admin@app-6pb7g.mongodb.net/test?retryWrites=true&w=majority';
+var DATABASE_NAME = 'app-db';
 
 var indexRouter = require('./routes/index');
 var filmsRouter = require('./routes/films');
 
 var app = express();
+var database;
+var client = new MongoClient(CONNECTION_URL, { useNewUrlParser: true });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+client.connect(err => {
+  if (err) {
+    throw err;
+  }
+  database = client.db(DATABASE_NAME);
+  console.log('Connected to `' + DATABASE_NAME + '`!');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use('/api/hello-world', indexRouter);
-app.use('/api/films', filmsRouter);
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({extended: false}));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  next(createError(404));
+  app.use('/api/hello-world', indexRouter);
+  app.use('/api/films', filmsRouter);
+
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+  });
 });
 
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
