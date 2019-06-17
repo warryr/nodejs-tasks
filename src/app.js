87f07be59da14637bb680cc290e00018fe6a4d90
warryr <heartsyrop@gmail.com>
2019-06-17 20:02:58 +0300

    fix: fix database connection

--- src/app.js
+++ src/app.js
@@ -2,39 +2,38 @@ import express from 'express';
 import path from 'path';
 import createError from 'http-errors';
 import logger from 'morgan';
-import { initDatabase, getDatabase } from './db';
+import initDatabase from './db';
 
 import indexRouter from './routes';
 import filmsRouterInitializer from './routes/films';
 
 const app = express();
 
-initDatabase(err => {
+initDatabase((db, err) => {
   if (err) throw err;
-});
-const db = getDatabase();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/hello-world', indexRouter);
-app.use('/api/films', filmsRouterInitializer(db));
+  app.use('/api/hello-world', indexRouter);
+  app.use('/api/films', filmsRouterInitializer(db));
 
-app.use((req, res, next) => {
-  next(createError(404));
-});
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error');
+  });
 });
 
 module.exports = app;
--- src/db.js
+++ src/db.js
@@ -5,20 +5,18 @@ const DATABASE_NAME = 'app-db';
 
 let _db;
 
-export const initDatabase = callback => {
+const initDatabase = async callback => {
   if (_db) {
     console.warn('Trying to init database again!');
-    return callback(null, _db);
+    return callback(_db, null);
   }
 
-  MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (err, client) => {
-    if (err) return callback(err);
+  await MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (err, client) => {
+    if (err) return callback(null, err);
     _db = client.db(DATABASE_NAME);
     console.log('Connected to `' + DATABASE_NAME + '`!');
-    return callback(null, _db);
+    return callback(_db, null);
   });
 };
 
-export const getDatabase = () => {
-  return _db;
-};
+export default initDatabase;
