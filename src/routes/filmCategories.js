5c9b92bc11d03c92478b759ecad3aced771119a5
warryr <heartsyrop@gmail.com>
2019-06-19 22:29:26 +0300

    feat: rewrite CRUD with mongoose

--- src/routes/filmCategories.js
+++ src/routes/filmCategories.js
@@ -1,83 +1,75 @@
 import express from 'express';
-import { ObjectID } from 'mongodb';
+import mongoose from 'mongoose';
 import { validate } from 'express-jsonschema';
+
 import FilmCategorySchema from '../jsonSchemas/filmCategorySchema';
+import FilmCategory from '../models/filmCategory';
 
 const router = express.Router();
 
-const initializeRouter = db => {
-  router.get('/', (req, res, next) => {
-    db.collection('film-categories')
-      .find(
-        {},
-        {
-          id: '$_id',
-          _id: 0,
-          title: 1,
-          description: 1,
-          films: 1,
-        }
-      )
-      .toArray((err, doc) => {
-        if (err) throw err;
-        res.send(doc);
-      });
-  });
-
-  router.post('/', validate({ body: FilmCategorySchema }), (req, res, next) => {
-    db.collection('film-categories').insertOne(req.body, (err, doc) => {
+router.get('/', (req, res, next) => {
+  FilmCategory.find(
+    {},
+    {
+      id: '$_id',
+      _id: 0,
+      title: 1,
+      description: 1,
+      films: 1,
+    },
+    (err, docs) => {
       if (err) throw err;
-      const category = doc.ops[0];
+      res.send(docs);
+    }
+  );
+});
+
+router.post('/', validate({ body: FilmCategorySchema }), (req, res, next) => {
+  FilmCategory.create(
+    {
+      _id: mongoose.Types.ObjectId(),
+      ...req.body,
+    },
+    (err, category) => {
+      if (err) throw err;
       category.id = category._id;
       delete category._id;
       res.send(category);
-    });
-  });
+    }
+  );
+});
 
-  router.put('/:id', validate({ body: FilmCategorySchema }), (req, res, next) => {
-    db.collection('film-categories').findOneAndUpdate(
-      { _id: ObjectID(req.params.id) },
-      { $set: req.body },
-      { returnOriginal: false },
-      (err, doc) => {
-        if (err) throw err;
-        doc.value ? res.send(doc.value) : res.status(404).json({ error: "requested id doesn't match category object" });
-      }
-    );
+router.put('/:id', validate({ body: FilmCategorySchema }), (req, res, next) => {
+  FilmCategory.findByIdAndUpdate(req.params.id, req.body,{ new: true }, (err, category) => {
+    if (err) throw err;
+    category ? res.send(category) : res.status(404).json({ error: "requested id doesn't match category object" });
   });
+});
 
-  router.delete('/:id', (req, res, next) => {
-    db.collection('film-categories').deleteOne(
-      {
-        _id: ObjectID(req.params.id),
-      },
-      (err, doc) => {
-        if (err) throw err;
-        if (doc.deletedCount) {
-          res.json({
-            success: true,
-            id: req.params.id,
-          });
-        } else {
-          res.status(404).json({
-            success: false,
-          });
-        }
-      }
-    );
-  });
-
-  router.use((err, req, res, next) => {
-    if (err.name === 'JsonSchemaValidation') {
-      const errors = {};
-      err.validations.body.map(object => (errors[object.property] = object.messages[0]));
-      res.status(400).send(errors);
+router.delete('/:id', (req, res, next) => {
+  FilmCategory.findByIdAndDelete(req.params.id, {}, (err, category) => {
+    if (err) throw err;
+    if (category) {
+      res.json({
+        success: true,
+        id: req.params.id,
+      });
     } else {
-      next(err);
+      res.status(404).json({
+        success: false,
+      });
     }
   });
+});
 
-  return router;
-};
+router.use((err, req, res, next) => {
+  if (err.name === 'JsonSchemaValidation') {
+    const errors = {};
+    err.validations.body.map(object => (errors[object.property] = object.messages[0]));
+    res.status(400).send(errors);
+  } else {
+    next(err);
+  }
+});
 
-export default initializeRouter;
+export default router;
--- src/routes/films.js
+++ src/routes/films.js
@@ -1,123 +1,84 @@
 import express from 'express';
-import { ObjectID } from 'mongodb';
+import mongoose from 'mongoose';
 import { validate } from 'express-jsonschema';
+
 import FilmSchema from '../jsonSchemas/filmSchema';
 import Film from '../models/film';
 
-import categoriesRouterInitializer from './filmCategories';
+import categoriesRouter from './filmCategories';
 
 const router = express.Router();
 
-const initializeRouter = () => {
-  router.get('/', (req, res, next) => {
-    const query = req.query.category ? { category: req.query.category } : {};
-
-    console.log('GET handling');
-
-    Film.find(
-      query,
-      {
-        id: '$_id',
-        _id: 0,
-        title: 1,
-        description: 1,
-        avatar: 1,
-        gallery: 1,
-        rating: 1,
-        category: 1,
-      },
-      { $limit: 10 },
-      (err, docs) => {
-        if (err) throw err;
-        res.send(docs);
-      }
-    );
-
-    /*db.collection('films')
-      .find(query, {
-        id: '$_id',
-        _id: 0,
-        title: 1,
-        description: 1,
-        avatar: 1,
-        gallery: 1,
-        rating: 1,
-        category: 1,
-      })
-      .limit(10)
-      .toArray((err, doc) => {
-        if (err) throw err;
-        res.send(doc);
-      });*/
-  });
-
-  router.post('/', validate({ body: FilmSchema }), (req, res, next) => {
-
-    console.log('POST handling');
-
-    Film.create(req.body, (err, doc) => {
+router.get('/', (req, res, next) => {
+  const query = req.query.category ? { category: req.query.category } : {};
+  Film.find(
+    query,
+    {
+      id: '$_id',
+      _id: 0,
+      title: 1,
+      description: 1,
+      avatar: 1,
+      gallery: 1,
+      rating: 1,
+      category: 1,
+    },
+    { $limit: 10 },
+    (err, docs) => {
       if (err) throw err;
-      const film = doc.ops[0];
+      res.send(docs);
+    }
+  );
+});
+
+router.post('/', validate({ body: FilmSchema }), (req, res, next) => {
+  Film.create(
+    {
+      _id: mongoose.Types.ObjectId(),
+      ...req.body,
+    },
+    (err, film) => {
+      if (err) throw err;
       film.id = film._id;
       delete film._id;
       res.send(film);
-    });
+    }
+  );
+});
 
-    /*db.collection('films').insertOne(req.body, (err, doc) => {
-      if (err) throw err;
-      const film = doc.ops[0];
-      film.id = film._id;
-      delete film._id;
-      res.send(film);
-    });*/
+router.put('/:id', validate({ body: FilmSchema }), (req, res, next) => {
+  Film.findByIdAndUpdate(req.params.id, req.body,{ new: true }, (err, film) => {
+    if (err) throw err;
+    film ? res.send(film) : res.status(404).json({ error: "requested id doesn't match film object" });
   });
+});
 
-  /*router.put('/:id', validate({ body: FilmSchema }), (req, res, next) => {
-    db.collection('films').findOneAndUpdate(
-      { _id: ObjectID(req.params.id) },
-      { $set: req.body },
-      { returnOriginal: false },
-      (err, doc) => {
-        if (err) throw err;
-        doc.value ? res.send(doc.value) : res.status(404).json({ error: "requested id doesn't match category object" });
-      }
-    );
-  });*/
-
-  /*router.delete('/:id', (req, res, next) => {
-    db.collection('films').deleteOne(
-      {
-        _id: ObjectID(req.params.id),
-      },
-      (err, doc) => {
-        if (err) throw err;
-        if (doc.deletedCount) {
-          res.json({
-            success: true,
-            id: req.params.id,
-          });
-        } else {
-          res.status(404).json({
-            success: false,
-          });
-        }
-      }
-    );
-  });*/
-
-  router.use('/categories', categoriesRouterInitializer);
-
-  router.use((err, req, res, next) => {
-    if (err.name === 'JsonSchemaValidation') {
-      const errors = {};
-      err.validations.body.map(object => (errors[object.property] = object.messages[0]));
-      res.status(400).send(errors);
+router.delete('/:id', (req, res, next) => {
+  Film.findByIdAndDelete(req.params.id, {}, (err, film) => {
+    if (err) throw err;
+    if (film) {
+      res.json({
+        success: true,
+        id: req.params.id,
+      });
     } else {
-      next(err);
+      res.status(404).json({
+        success: false,
+      });
     }
   });
+});
 
-  return router;
-};
+router.use('/categories', categoriesRouter);
 
-export default initializeRouter;
+router.use((err, req, res, next) => {
+  if (err.name === 'JsonSchemaValidation') {
+    const errors = {};
+    err.validations.body.map(object => (errors[object.property] = object.messages[0]));
+    res.status(400).send(errors);
+  } else {
+    next(err);
+  }
+});
+
+export default router;
