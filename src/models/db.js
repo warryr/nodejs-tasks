f5bea6196572bed8319d22fa004e6c06fa5e8691
warryr <heartsyrop@gmail.com>
2019-06-19 22:26:36 +0300

    feat: rewrite db connection with mongoose

---
+++ src/models/db.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const CONNECTION_URL = 'mongodb+srv://admin:admin@app-6pb7g.mongodb.net/test?retryWrites=true&w=majority';
+const DATABASE_NAME = 'app-db';
+
+export default function() {
+  mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, dbName: DATABASE_NAME });
+  const connection = mongoose.connection;
+
+  connection.on('connected', function () {
+    console.log('Connected to ' + DATABASE_NAME);
+  });
+
+  connection.on('error',function (err) {
+    console.log('Connection error: ' + err);
+  });
+
+  connection.on('disconnected', function () {
+    console.log('Disconnected from ' + DATABASE_NAME);
+  });
+
+  process.on('SIGINT', function(){
+    mongoose.connection.close(function(){
+      console.log("Mongoose default connection is disconnected due to application termination");
+      process.exit(0);
+    });
+  });
+};
