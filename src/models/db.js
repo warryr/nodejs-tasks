138ac526a25e91fb7909742d254f99bbe4f3c6da
warryr <heartsyrop@gmail.com>
2019-06-19 22:28:39 +0300

    refactor: refactor with prettier

--- src/models/db.js
+++ src/models/db.js
@@ -7,22 +7,22 @@ export default function() {
   mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, dbName: DATABASE_NAME });
   const connection = mongoose.connection;
 
-  connection.on('connected', function () {
+  connection.on('connected', function() {
     console.log('Connected to ' + DATABASE_NAME);
   });
 
-  connection.on('error',function (err) {
+  connection.on('error', function(err) {
     console.log('Connection error: ' + err);
   });
 
-  connection.on('disconnected', function () {
+  connection.on('disconnected', function() {
     console.log('Disconnected from ' + DATABASE_NAME);
   });
 
-  process.on('SIGINT', function(){
-    mongoose.connection.close(function(){
-      console.log("Mongoose default connection is disconnected due to application termination");
+  process.on('SIGINT', function() {
+    mongoose.connection.close(function() {
+      console.log('Mongoose default connection is disconnected due to application termination');
       process.exit(0);
     });
   });
-};
+}
