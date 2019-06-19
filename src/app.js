44055769033f01696e4b91626ef7b251d71009c4
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-06-19 14:35:39 +0300

    feat: attempt to setup mongoose connection

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ import express from 'express';
 import path from 'path';
 import createError from 'http-errors';
 import logger from 'morgan';
-import initDatabase from './db';
+//import initDatabase from './db';
+import mongoose from 'mongoose';
+
+const CONNECTION_URL = 'mongodb+srv://admin:admin@app-6pb7g.mongodb.net/test?retryWrites=true&w=majority';
+const DATABASE_NAME = 'app-db';
 
 import indexRouter from './routes';
 import filmsRouterInitializer from './routes/films';
 
 const app = express();
 
-initDatabase((db, err) => {
-  if (err) throw err;
+// initDatabase((db, err) => {
+//   if (err) throw err;
+//   // db is passed to router initializers
+// });
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, dbName: DATABASE_NAME });
+const connection = mongoose.connection;
+
+connection.on('connected', function () {
+  console.log('Connected to ' + DATABASE_NAME);
 
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
@@ -21,7 +33,7 @@ initDatabase((db, err) => {
   app.use(express.static(path.join(__dirname, 'public')));
 
   app.use('/api/hello-world', indexRouter);
-  app.use('/api/films', filmsRouterInitializer(db));
+  app.use('/api/films', filmsRouterInitializer);
 
   app.use((req, res, next) => {
     next(createError(404));
@@ -34,6 +46,14 @@ initDatabase((db, err) => {
     res.status(err.status || 500);
     res.render('error');
   });
+});
+
+connection.on('error',function (err) {
+  console.log('Connection error: ' + err);
+});
+
+connection.on('disconnected', function () {
+  console.log('Disconnected from ' + DATABASE_NAME);
 });
 
 module.exports = app;
--- src/routes/films.js
+++ src/routes/films.js
@@ -2,15 +2,38 @@ import express from 'express';
 import { ObjectID } from 'mongodb';
 import { validate } from 'express-jsonschema';
 import FilmSchema from '../jsonSchemas/filmSchema';
+import Film from '../models/film';
 
 import categoriesRouterInitializer from './filmCategories';
 
 const router = express.Router();
 
-const initializeRouter = db => {
+const initializeRouter = () => {
   router.get('/', (req, res, next) => {
     const query = req.query.category ? { category: req.query.category } : {};
-    db.collection('films')
+
+    console.log('GET handling');
+
+    Film.find(
+      query,
+      {
+        id: '$_id',
+        _id: 0,
+        title: 1,
+        description: 1,
+        avatar: 1,
+        gallery: 1,
+        rating: 1,
+        category: 1,
+      },
+      { $limit: 10 },
+      (err, docs) => {
+        if (err) throw err;
+        res.send(docs);
+      }
+    );
+
+    /*db.collection('films')
       .find(query, {
         id: '$_id',
         _id: 0,
@@ -25,20 +48,31 @@ const initializeRouter = db => {
       .toArray((err, doc) => {
         if (err) throw err;
         res.send(doc);
-      });
+      });*/
   });
 
   router.post('/', validate({ body: FilmSchema }), (req, res, next) => {
-    db.collection('films').insertOne(req.body, (err, doc) => {
+
+    console.log('POST handling');
+
+    Film.create(req.body, (err, doc) => {
       if (err) throw err;
       const film = doc.ops[0];
       film.id = film._id;
       delete film._id;
       res.send(film);
     });
+
+    /*db.collection('films').insertOne(req.body, (err, doc) => {
+      if (err) throw err;
+      const film = doc.ops[0];
+      film.id = film._id;
+      delete film._id;
+      res.send(film);
+    });*/
   });
 
-  router.put('/:id', validate({ body: FilmSchema }), (req, res, next) => {
+  /*router.put('/:id', validate({ body: FilmSchema }), (req, res, next) => {
     db.collection('films').findOneAndUpdate(
       { _id: ObjectID(req.params.id) },
       { $set: req.body },
@@ -48,9 +82,9 @@ const initializeRouter = db => {
         doc.value ? res.send(doc.value) : res.status(404).json({ error: "requested id doesn't match category object" });
       }
     );
-  });
+  });*/
 
-  router.delete('/:id', (req, res, next) => {
+  /*router.delete('/:id', (req, res, next) => {
     db.collection('films').deleteOne(
       {
         _id: ObjectID(req.params.id),
@@ -69,9 +103,9 @@ const initializeRouter = db => {
         }
       }
     );
-  });
+  });*/
 
-  router.use('/categories', categoriesRouterInitializer(db));
+  router.use('/categories', categoriesRouterInitializer);
 
   router.use((err, req, res, next) => {
     if (err.name === 'JsonSchemaValidation') {
